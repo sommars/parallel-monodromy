252fbbf2d49ebf14fbba138fcaee276687ffe623
Tim <timothy.duff@ncf.edu>
2018-04-25 15:28:54 -0400

    removed duplicated code in autotest

--- code/autotest.sh
+++ code/autotest.sh
@@ -1,23 +1,7 @@
-rm ~/outorig.txt
-rm ~/outnew.txt
-
-echo "simple test for 1 thread"
-./pmonodromy ./graph-examples/random.graph -s 0 -old >> ~/outorig.txt
-./pmonodromy ./graph-examples/random.graph -s 0 >> ~/outnew.txt
+echo "simple test for $1 thread on seed $2"
+./pmonodromy ./graph-examples/random.graph -s $2 -t $1 -old >> ~/outorig.txt
+./pmonodromy ./graph-examples/random.graph -s $2 -t $1 >> ~/outnew.txt
 diff ~/outorig.txt ~/outnew.txt
 
-rm ~/outorig.txt
-rm ~/outnew.txt
-
-echo "simple test for 2 threads"
-./pmonodromy ./graph-examples/random.graph -s 0 -t 2 -old >> ~/outorig.txt
-./pmonodromy ./graph-examples/random.graph -s 0 -t 2 >> ~/outnew.txt
-diff ~/outorig.txt ~/outnew.txt
-
-rm ~/outorig.txt
-rm ~/outnew.txt
-
-echo "simple test for 14 threads"
-./pmonodromy ./graph-examples/random.graph -s 0 -t 14 -old >> ~/outorig.txt
-./pmonodromy ./graph-examples/random.graph -s 0 -t 14 >> ~/outnew.txt
-diff ~/outorig.txt ~/outnew.txt
+rm ./outorig.txt
+rm ./outnew.txt
